6e162ce7e64d2999e219f05faa47112605cefc9d
Neil Herath <146669388+Hyemivl@users.noreply.github.com>
2023-11-02 08:10:57 +0000

    Coursework 1 1012 finished

--- AssessmentWork/1012/cwk1.py
+++ AssessmentWork/1012/cwk1.py
@@ -11,32 +11,67 @@ def valid_puzzle(puzzle: list) -> bool:
     for x in puzzle:
         if(len(x) != length):
             output = False
-        if not((isinstance(x,str))):
-            output = False
-    print(len(puzzle))
-    if(len(puzzle) <= 1):
+    print(len(puzzle[0]))
+    if(len(puzzle[0]) <= 1):
         output = False
     return output
 
 
 def similarity_grouping(data: list) -> list:
-    # delete this line and pass to write your code here
-    pass
+    temp = ' '
+    templist = []
+    output = []
+    counter = 0
+    if isinstance(data, list):
+        while data != []:
+            temp = data[0]
+            while (counter < len(data)):
+                if temp == data[counter]:
+                    data.remove(temp)
+                    templist.append(temp)
+                    counter = counter - 1
+                counter = counter + 1
+            output.append(templist)
+            counter = 0
+            templist = []
+    return output
 
 
 def highest_count_items(data: str) -> list:
-    # delete this line and pass to write your code here
-    pass
+    listofcount = []
+    dictionaryofcount = {}
+    output = []
+    if isinstance(data, str):
+        strlist = (data.split(", "))
+        for x in strlist:
+            listofcount.append(strlist.count(x))
+            dictionaryofcount[x] = strlist.count(x)
+        highestcount = max(listofcount)
+        for x,y in dictionaryofcount.items():
+            if(y==highestcount):
+                output.append([x,y])
+    return output
 
 
 def valid_char_in_string(popList: list, charSet: list) -> bool:
-    # delete this line and pass to write your code here
-    pass
+    output = True
+    if not isinstance(charSet, list):
+        return False
+    for x in charSet:
+        if (len(x) > 1) and isinstance(x, str):
+            return False
+    for x in popList:
+        if not all(c in charSet for c in x):
+            output = False
+    return output
 
 
 def total_price(unit: int) -> float:
-    # delete this line and pass to write your code here
-    pass
+    output = 0
+    output = 5 * (unit//6) + 1.25 * (unit%6)
+    if output>= 20:
+        output = output * 0.9
+    return output
 
 
 if __name__ == "__main__":
@@ -67,23 +102,23 @@ if __name__ == "__main__":
     print(similarity_grouping(data3))
 
     # sample test for task 1.3
-    data4 = ("3, 13, 7, 9, 3, 3, 5, 7, 12, 13, 11, 13, 8, 7, 5, 14, 15, 3, 9,"
-             "7, 5, 9, 14, 3, 8, 2, 5, 5, 8, 14, 11, 11, 12, 8, 5, 3, 3, 10,"
+    data4 = ("3, 13, 7, 9, 3, 3, 5, 7, 12, 13, 11, 13, 8, 7, 5, 14, 15, 3, 9, "
+             "7, 5, 9, 14, 3, 8, 2, 5, 5, 8, 14, 11, 11, 12, 8, 5, 3, 3, 10, "
              "3, 10, 7, 7, 10, 10, 2, 7, 4, 8, 1, 5")
-    data5 = ("British Gas, British Gas, Ovo, Igloo, Igloo, Octopus, Bulb,"
-             "Shell, E.ON, Npower, British Gas, Octopus, Igloo, Npower, Igloo,"
-             "Shell, Scottish Power, Bulb, EDF, Bulb, EDF, Bulb,"
-             "Scottish Power, Octopus, Scottish Power, Octopus, Octopus, EDF,"
-             "Ovo, Shell, Octopus, E.ON, British Gas, Bulb, Npower, Shell,"
+    data5 = ("British Gas, British Gas, Ovo, Igloo, Igloo, Octopus, Bulb, "
+             "Shell, E.ON, Npower, British Gas, Octopus, Igloo, Npower, Igloo, "
+             "Shell, Scottish Power, Bulb, EDF, Bulb, EDF, Bulb, "
+             "Scottish Power, Octopus, Scottish Power, Octopus, Octopus, EDF, "
+             "Ovo, Shell, Octopus, E.ON, British Gas, Bulb, Npower, Shell, "
              "Scottish Power, Npower, Scottish Power, Npower")
-    data6 = ("aac, ctt, gat, ccc, gcc, ctg, gtc, tcg, ccg, cca, ata, cca,"
-             "tat, ata, cac, gcg, cca, gta, gtg, gac, taa, ata, gtc, aat, gct,"
-             "gta, ggc, tgc, tca, ctt, tgt, ata, acc, tgc, gac, cgc, atc, cgt,"
-             "tac, agg, ctt, cgc, cgc, tgg, gcg, tgg, taa, cta, aaa, tgc, cgt,"
-             "tgc, gac, tta, aag, taa, act, cca, tac, agg, cgc, gtg, cca, gcg,"
-             "gtt, gag, tca, aca, tct, gta, ata, ctt, gat, tcg, tcg, cac, cgt,"
-             "tac, caa, aac, ctg, tgt, aag, ttc, ccc, tcc, ctc, cct, aga, gtt,"
-             "tga, gaa, cct, ctc, tct, ggt, gcc, tct, ccc, agt, caa, gac, ccc,"
+    data6 = ("aac, ctt, gat, ccc, gcc, ctg, gtc, tcg, ccg, cca, ata, cca, "
+             "tat, ata, cac, gcg, cca, gta, gtg, gac, taa, ata, gtc, aat, gct, "
+             "gta, ggc, tgc, tca, ctt, tgt, ata, acc, tgc, gac, cgc, atc, cgt, "
+             "tac, agg, ctt, cgc, cgc, tgg, gcg, tgg, taa, cta, aaa, tgc, cgt, "
+             "tgc, gac, tta, aag, taa, act, cca, tac, agg, cgc, gtg, cca, gcg, "
+             "gtt, gag, tca, aca, tct, gta, ata, ctt, gat, tcg, tcg, cac, cgt, "
+             "tac, caa, aac, ctg, tgt, aag, ttc, ccc, tcc, ctc, cct, aga, gtt, "
+             "tga, gaa, cct, ctc, tct, ggt, gcc, tct, ccc, agt, caa, gac, ccc, "
              "cgc")
     print(highest_count_items(data4))
     print(highest_count_items(data5))
