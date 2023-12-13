371b1ffbc4fcda7afaccba8e8c451aad38fc4572
Neil Herath <146669388+Hyemivl@users.noreply.github.com>
2023-12-13 14:30:52 +0000

    Assessment work

--- AssessmentWork/1711/StepCounter.c
+++ AssessmentWork/1711/StepCounter.c
@@ -12,7 +12,10 @@
 int buffer_size = 50;
 // a string to hold each line making it the same size as the buffer size
 char line_buffer[50];
+// a multi purpose counter used in the switch statement
 int counter = 0;
+// a variable to hold the output of some functions
+FITNESS_DATA output;
 // hold temp variables to store tokenised values
 char date[10];
 char time[10];
@@ -57,7 +60,7 @@ void menu()
     printf("E: Find the mean step count of all the records in the file\n");
     printf("F: Find the longest continuous period where the step count is above 500 steps\n");
     printf("Q: Quit\n");
-    printf("Enter option: ");
+    printf("Enter option:");
 }
 
 int FileExists(FILE *file,char filename[])
@@ -82,9 +85,8 @@ int main()
     while(1)
     {
         menu();
-        input = getchar();
+        input = getchar();        
         while (getchar() != '\n');
-        printf("%c",input);
         switch (input)
         {
         case 'a':
@@ -97,6 +99,7 @@ int main()
                 printf("Error opening file\n");
                 return 1;
             }
+            while (getchar() != '\n');
             break;
         case 'b':
         case 'B':
@@ -121,15 +124,153 @@ int main()
             break;
         case 'c':
         case 'C':
+            if(FileExists(file,filename) == 0)
+            {
+                file = fopen(filename,"r");
+                {
+                    // Counter set to high value so that every value will be below it to find the minimum
+                    counter = 10000;
+                    // Reads file
+                    while (fgets(line_buffer, buffer_size, file) != NULL)
+                    {
+                        tokeniseRecord(line_buffer,",",date,time,steps);
+                        if(counter > atoi(steps))
+                        {
+                            counter = atoi(steps);
+                            strcpy(output.date,date);
+                            strcpy(output.time,time);
+                            output.steps = atoi(steps);
+                        }
+                    }
+                fclose(file);
+                }
+                printf("Fewest steps: %s %s\n",output.date,output.time);
+                counter = 0;
+            }
+            else
+            {
+                printf("No valid filename selected.\n");
+            }
             break;
         case 'd':
         case 'D':
+            if(FileExists(file,filename) == 0)
+            {
+                file = fopen(filename,"r");
+                {
+                    // Reads file
+                    while (fgets(line_buffer, buffer_size, file) != NULL)
+                    {
+                        tokeniseRecord(line_buffer,",",date,time,steps);
+                        if(counter < atoi(steps))
+                        {
+                            counter = atoi(steps);
+                            strcpy(output.date,date);
+                            strcpy(output.time,time);
+                            output.steps = atoi(steps);
+                        }
+                    }
+                fclose(file);
+                }
+                printf("Largest steps: %s %s\n",output.date,output.time);
+                counter = 0;
+            }
+            else
+            {
+                printf("No valid filename selected.\n");
+            }
             break;
         case 'e':
         case 'E':
+            if(FileExists(file,filename) == 0)
+            {
+                double mean = 0;
+                file = fopen(filename,"r");
+                {
+                    // Reads file
+                    while (fgets(line_buffer, buffer_size, file) != NULL)
+                    {
+                        tokeniseRecord(line_buffer,",",date,time,steps);
+                        counter++;
+                        mean += atoi(steps);
+                    }
+                fclose(file);
+                }
+                mean =  (mean / counter);
+                printf("Mean step count: %.0f\n",mean);
+                counter = 0;
+            }
+            else
+            {
+                printf("No valid filename selected.\n");
+            }
             break;
         case 'f':
         case 'F':
+            if(FileExists(file,filename) == 0)
+            {
+                FITNESS_DATA start;
+                FITNESS_DATA end;
+                int length = 0;
+                int maxlength = 0;
+                counter = 0;
+                file = fopen(filename,"r");
+                {
+                    // Reads file
+                    while (fgets(line_buffer, buffer_size, file) != NULL)
+                    {
+                        tokeniseRecord(line_buffer,",",date,time,steps);
+                        strcpy(data[counter].date,date);
+                        strcpy(data[counter].time,time);
+                        data[counter].steps = atoi(steps);
+                        counter++;
+                    }
+                fclose(file);
+                }
+                for (int i = 0; i < counter; i++)
+                {
+                    if (data[i].steps > 500)
+                    {
+                        length++;
+                    }
+                    else
+                    {
+                        length = 0;
+                    }
+                    if(length > maxlength)
+                    {
+                        maxlength = length;
+                    }
+                }
+                for (size_t i = 0; i < counter; i++)
+                {
+                    if(data[i].steps > 500 && length > 0)
+                    {
+                        end = data[i];
+                        length ++;
+                    }
+                    else if(data[i].steps > 500)
+                    {
+                        start = data[i];
+                        length = 1;
+                    }
+                    else
+                    {
+                        length = 0;
+                    }
+                    if (length == maxlength)
+                    {
+                        printf("Longest period start: %s %s\n",start.date,start.time);
+                        printf("Longest period end: %s %s\n",end.date,end.time);
+                    }
+                }
+                
+                counter = 0;
+            }
+            else
+            {
+                printf("No valid filename selected.\n");
+            }
             break;
         case 'q':
         case 'Q':
